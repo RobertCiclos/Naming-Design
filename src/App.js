0c585ffce2617483da57cc09bc043e01e608f60b
Ivan <carlosivanriveraespilco@gmail.com>
2022-09-06 11:44:59 -0500

    colores y fuentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      Hola zzzzz
+    <div className="App">
+      <Navbar />
+      <Home />
+      <Footer />
     </div>
   );
 }
