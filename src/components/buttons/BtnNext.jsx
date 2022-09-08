6a7a2e7d61169c563b8a8de232e01f23a6d71d0a
natvvgls <natalia.d.m975@gmail.com>
2022-09-08 12:43:59 -0500

    Componente btnnext

---
+++ src/components/buttons/BtnNext.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BtnNext = () => {
+    return (
+        <button>
+
+        </button>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
 const Home = () => {
-    const dbHome = require("../bd/home.json")
-    console.log(dbHome)
+  
   return (
     <section>
       <h1>
